86af3b2759a256e40a94da2319be7807ce32f6ee
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-02-24 16:31:31 +0000

    Dependency Injection using Spring Framework, Version annotations

--- src/main/java/ma/app/dao/DaoImpl.java
+++ src/main/java/ma/app/dao/DaoImpl.java
@@ -1,5 +1,8 @@
 package ma.app.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component("dao")
 public class DaoImpl implements IDao{
 
     @Override
--- src/main/java/ma/app/metier/MetierImpl.java
+++ src/main/java/ma/app/metier/MetierImpl.java
@@ -1,9 +1,12 @@
 package ma.app.metier;
 
 import ma.app.dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("metier")
 public class MetierImpl implements IMetier{
-
+    @Autowired
     private IDao dao;
 
     public void setDao(IDao dao) {
